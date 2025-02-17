bcaa6cdd3a407de8bcad531be6e3d1901c8c7fc0
david-kasprak <kasprakdavid@gmail.com>
2025-02-17 16:38:34 +0200

    DONE (start by running terminal command: "docker compose up --build", and then go to localhost:81

---
+++ backend/src/dataBase/userModel.js
@@ -0,0 +1,14 @@
+const {Schema, Model, models, model} = require('mongoose');
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    }
+})
+
+module.exports = model('user', userSchema)
